fed28cd3d5b2dfec779aee0e403fd27223ebfe41
Jon Janelle <jonjanelle1@gmail.com>
2020-04-01 07:53:49 -0400

    added geocoding script

---
+++ scripts/pantry_geocode.py
@@ -0,0 +1,36 @@
+import requests
+import csv
+import json
+
+geocodingService = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?f=pjson&SingleLine='
+singleLineAddress = ''
+
+newData = []
+
+with open('pantries_me_original.csv', newline='', encoding="utf8") as csvfile:
+    csvReader = csv.reader(csvfile)
+    count = 0
+    for row in csvReader:
+        if count == 0:
+            row.append('latitude')
+            row.append('longitude')
+        elif count > 0:
+            r = requests.get(geocodingService + row[4])
+            candidates = r.json()['candidates']
+            if len(candidates) > 0:
+                bestMatch = candidates[0]
+                row.append(bestMatch['location']['y']) # latitude
+                row.append(bestMatch['location']['x'])
+                print(f"Finished {bestMatch['address']} {bestMatch['location']['y']} {bestMatch['location']['x']}")
+            #input('key to continue')
+
+        newData.append(row)
+        count += 1
+
+print("Writing data...")
+with open('output.csv', 'w', newline='', encoding="utf8") as csvfile:
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+    for row in newData:
+        writer.writerow(row)
+
+print("Done.")
