7024ccb36f7ac4dc050c9411868ef5bb7503ae7f
Jon Janelle <jonjanelle1@gmail.com>
2020-04-05 12:01:55 -0400

    increase zoom level when using device location

--- js/defaultLocation.js
+++ js/defaultLocation.js
@@ -1,9 +1,11 @@
 function getDefaultLocation() {
     return new Promise((resolve) => {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(x => resolve(x.coords));
+            navigator.geolocation.getCurrentPosition(x => resolve(x.coords), (err) => resolve(null));
         } else {
             resolve(null)
         }
-    })
+    }, (err) => {
+        resolve(null);
+    });
 }
