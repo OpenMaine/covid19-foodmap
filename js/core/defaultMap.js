1939489f08c9643b2a70664ee354f00b3aa6af22
Jon Janelle <jonjanelle1@gmail.com>
2020-04-10 17:03:47 -0400

    remove geocoding from default map

--- js/core/defaultMap.js
+++ js/core/defaultMap.js
@@ -1,5 +1,4 @@
 class DefaultMap {
-    _geocodingService = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"; //remove after switch to Geocoder
     _mapboxToken = 'pk.eyJ1Ijoiam9uamFuZWxsZSIsImEiOiJjazhxbXg0YmswNW5kM2RvNGNjb2hiN2poIn0.LiFKVlPQe_vqyqjjIw0DIw';
     markers = {};
     
@@ -60,42 +59,5 @@ class DefaultMap {
             var markerBounds = L.latLngBounds([Object.values(this.markers)[0].getLatLng()]);
             this.map.fitBounds(markerBounds);
         }
-    }
-
-    getZipcodeGeopoint(zipCode) {
-        const deferred = $.Deferred();
-
-        const match = zipCode.toString().trim().match(/^([0-9]{5})(-[0-9]{4})?$/);
-        if (match){
-            const requestUri = `${this._geocodingService}/findAddressCandidates?f=json&SingleLine=${match[1]}`;
-            $.get(requestUri).done((response, status) => {
-                if (response.candidates && response.candidates.length > 0)
-                    deferred.resolve(new GeoPoint(response.candidates[0].location.y, response.candidates[0].location.x));
-                else
-                    deferred.reject();
-            });
-        } else {
-            deferred.reject();
-        }
-
-        return deferred.promise();
-    }
-
-    reverseGeocode(geoPoint) {
-        const deferred = $.Deferred();
-
-        if (geoPoint.latitude && geoPoint.longitude) {
-            const requestUri = `${this._geocodingService}/reverseGeocode?location=${geoPoint.longitude},${geoPoint.latitude}&forStorage=false&f=json&featureTypes=PointAddress`;
-            $.get((requestUri)).done((response, status) => {
-                if (response.address && response.address.City && response.address.Subregion)
-                    deferred.resolve([response.address.City, response.address.Subregion.split(" ")[0]]);
-                else 
-                    deferred.reject();
-            });
-        } else {
-            deferred.reject();
-        }
-
-        return deferred.promise();
     }
 }
